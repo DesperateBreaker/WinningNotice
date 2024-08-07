daae93a7d05fe8237e190eafbea748ece4d82b59
sun_jia_hao <sun_jia_hao@sina.com>
2024-08-07 11:11:55 +0000

    feat: preservation

---
+++ src/json.py
@@ -0,0 +1,26 @@
+# 本模块用于读和写 json 文件
+import json
+
+def read_json_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        print(f"文件 {file_path} 未找到。")
+        return None
+    except json.JSONDecodeError:
+        print(f"文件 {file_path} 不是有效的JSON格式。")
+        return None
+    except Exception as e:
+        print(f"读取文件 {file_path} 时发生错误：{e}")
+        return None
+
+
+def write_json_file(file_path, data):
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+        print(f"数据已成功写入到文件 {file_path}")
+    except Exception as e:
+        print(f"写入文件 {file_path} 时发生错误：{e}")
---
+++ src/statistics.py
@@ -0,0 +1,7 @@
+# 负责一些信息的统计工作
+
+
+
+class Statistics:
+    def __init__(self):
+        self.id = 0  
