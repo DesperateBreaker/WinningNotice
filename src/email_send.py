94e538f7062b315434ae60775136440de8492ae3
DesperateBreaker <sun_jia_hao@sina.com>
2024-10-08 21:32:12 +0800

    feat: 修改邮件发送

--- src/email_send.py
+++ src/email_send.py
@@ -22,18 +22,17 @@ def send_email(sender_email, sender_password, receiver_email_list, subject, mess
     # 邮件正文
     msg.attach(MIMEText(message, 'plain', 'utf-8'))
 
-    # 连接 SMTP 服务器并发送邮件
-    try:
-        context = ssl.create_default_context()
-        server = smtplib.SMTP_SSL('smtp.qq.com', 465, context=context)  # 使用客户端 SSL 上下文
-        server.login(sender_email, sender_password)
-        
-        for receiver_email in receiver_email_list:
+    # TODO: for 理应写在 try 内，可以一次登录发送多个，但是暂时有问题就这样写
+    for receiver_email in receiver_email_list:
+        # 连接 SMTP 服务器并发送邮件
+        try:
+            context = ssl.create_default_context()
+            server = smtplib.SMTP_SSL('smtp.qq.com', 465, context=context)  # 使用客户端 SSL 上下文
+            server.login(sender_email, sender_password)
             msg['To'] = receiver_email
+
             server.sendmail(sender_email, receiver_email, msg.as_string())
             print(f"邮件发送成功至 {receiver_email}")
-        
-        server.quit()
-        print("邮件发送完毕")
-    except Exception as e:
-        print("邮件发送失败:", str(e))
+            server.quit()
+        except Exception as e:
+            print("邮件发送失败:", str(e))
